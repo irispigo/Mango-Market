b7fce67cd3ded920300d273a1e94e83a6b498fab
irispigo <162714059+irispigo@users.noreply.github.com>
2024-10-21 07:36:03 -0700

    Update GovernanceDelegate.tsx

--- components/governance/GovernanceDelegate.tsx
+++ components/governance/GovernanceDelegate.tsx
@@ -1,3 +1,4 @@
+import Select from '@components/forms/Select'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { PublicKey } from '@solana/web3.js'
 import GovernanceStore from '@store/governanceStore'
